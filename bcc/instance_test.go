e4cad98ff1a66ba6f62430394c560fcd55361e34
mengyang02 <mengyang02@baidu.com>
2018-11-23 20:20:36 +0800

    release eip if necessary when delete bcc

--- bcc/instance_test.go
+++ bcc/instance_test.go
@@ -2,6 +2,7 @@ package bcc
 
 import (
 	"testing"
+	"time"
 
 	"github.com/baidu/baiducloud-sdk-go/billing"
 )
@@ -14,22 +15,28 @@ func TestCreateInstance(t *testing.T) {
 		Billing: billing.Billing{
 			PaymentTiming: "Postpaid",
 		},
-		AdminPass:          "thisistestpw123!",
-		CPUCount:           1,
-		MemoryCapacityInGB: 1,
-		PurchaseCount:      1,
+		AdminPass:             "thisistestpw123!",
+		CPUCount:              1,
+		MemoryCapacityInGB:    1,
+		PurchaseCount:         1,
+		NetworkCapacityInMbps: 1,
 	}
-	intances, err := bccClient.CreateInstances(args, nil)
+	instances, err := bccClient.CreateInstances(args, nil)
 	if err != nil {
 		t.Errorf("Failed to create image, err: %+v", err)
 	}
-	t.Logf("Created instances: %+v", intances)
-
+	t.Logf("Created instances: %+v", instances)
+	time.Sleep(5 * time.Second)
+	ins, err := bccClient.DescribeInstance(instances[0], nil)
+	if err != nil {
+		t.Errorf("Describe instance err: %+v", err)
+	}
+	t.Logf("instance info: %+v", ins)
 }
 
 func TestDeleteIntsance(t *testing.T) {
 	bccClient := NewTestClient()
-	InstanceID := "i-eoWGc90b"
+	InstanceID := "i-EhnYCRaH"
 	err := bccClient.DeleteInstance(InstanceID, nil)
 	if err != nil {
 		t.Errorf("Failed to delete instance: %+v, err: %+v", InstanceID, err)
